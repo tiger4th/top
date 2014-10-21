b4f5dbc9433e7296d88a044ec017a75c36d1067f
tiger4th <tiger4th@gmail.com>
2014-10-21 14:37:44 +0900

    stable

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="content-Style-type" content="text/css" />
+<meta http-equiv="content-Script-type" content="text/javascript" />
+<title>tiger4th.com</title>
+<link rel="stylesheet" type="text/css" href="./style.css" />
+<link rel="shortcut icon" href="./image/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="icon" href="./image/favicon.ico" type="image/vnd.microsoft.icon" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20423739-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+
+<div class="centerMiddle"  align="center">
+<img src="./image/tiger4th.png" alt="tiger4th.com">
+<br /><br /><br />
+<a href="http://minrev.main.jp/"><img src="./image/minrev.png" alt="みんなの新着レビュー" height=50></a>
+<br /><br /><br />
+<a href="http://minrev.main.jp/c/"><img src="./image/minrevc.png" alt="みんなの新着レビューC" height=50></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/yamazon/"><img src="./image/yamazon.png" alt="Yamazon"></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/sc/"><img src="./image/sc.png" alt="social checker" height=50></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/anibuzz/"><img src="./image/anibuzz.png" alt="anibuzz" height=50></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/drabuzz/"><img src="./image/drabuzz.png" alt="drabuzz" height=50></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/hatebu/"><img src="./image/hatebu.png" alt="はてぶ新聞" height=60></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/hatebu_ex/"><img src="./image/hatebu_ex.png" alt="はてぶ新聞 速報版" height=60></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/sga/"><img src="./image/sga.png" alt="ソーシャルゲームオークション" height=30></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/auc/"><img src="./image/auc.png" alt="オークションシリーズ" height=30></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/snews/"><img src="./image/snews.png" alt="Social Newspaper" height=60></a>
+<br /><br /><br />
+<a href="http://tiger4th.com/sword/"><img src="./image/sword.png" alt="刀剣オークション" height=50></a>
+<br /><br /><br />
+<a href="others"><img src="./image/others.png" alt="others"></a>
+<br /><br /><br />
+<img src="./image/gmail.png"alt="gmail">
+<br /><br /><br />
+
+<!-- AddThis Button BEGIN -->
+<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=300&amp;pubid=ra-4e3ab77310f2fc55"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e3ab77310f2fc55"></script>
+<!-- AddThis Button END -->
+<br /><br /><br />
+</div>
+</body>
+</html>
---
+++ others/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="content-Style-type" content="text/css" />
+<meta http-equiv="content-Script-type" content="text/javascript" />
+<title>tiger4th.com : others</title>
+<link rel="stylesheet" type="text/css" href="./style.css" />
+<link rel="shortcut icon" href="../image/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="icon" href="../image/favicon.ico" type="image/vnd.microsoft.icon" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20423739-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+
+<div class="centerMiddle"  align="center">
+<a href="http://tiger4th.com/"><img src="../image/tiger4th.png" alt="tiger4th.com"></a>
+<br /><br /><br />
+
+過去に作ったプログラム置き場<br />
+すべてWindows用アプリケーション<br />
+<br />
+<hr>
+<p class="la">ウォルフラムのセルオートマトン</p>
+1次元セルオートマトン256種類をシミュレーションできます。<br />
+<br />
+<img src="./image/wolfram.png" alt="wolfram" height="400"><br />
+<br />
+<img src="./image/wolfram2.png" alt="wolfram2" height="200"><br />
+<br />
+<a href="./files/Wolfram.zip">ダウンロード</a><br />
+<br />
+<hr>
+<p class="la">ライフゲーム</p>
+標準ルール23/3のライフゲームです。<br />
+クリックでセルを置いたり消したりできます。<br />
+<br />
+<img src="./image/lifegame.png" alt="lifegame" height="400"><br />
+<br />
+<a href="./files/LifeGame.zip">ダウンロード</a><br />
+<br />
+<hr>
+<p class="la">セルオートマトンによる微分方程式の計算</p>
+セルオートマトンを使って微分方程式で表される現象のシミュレーションをします。<br />
+クリックするとそのセルの値を表示します。<br />
+初期配置が異なる3種類が入っています。<br />
+<br />
+<img src="./image/CA2D.PNG" alt="CA2D" height="400"><br />
+<br />
+<img src="./image/CA2D2.PNG" alt="CA2D2" height="200"><br />
+<br />
+<a href="./files/CA2D.zip">ダウンロード</a><br />
+<br />
+<hr>
+<p class="la">6角形セルオートマトン</p>
+セルを6角形にして6近傍の計算ルールにしたセルオートマトン。<br />
+「設定」はただの消し忘れで効きません。<br />
+<br />
+<img src="./image/hexa.png" alt="hexa" height="400"><br />
+<br />
+<a href="./files/hexa.zip">ダウンロード</a><br />
+<br />
+<hr>
+<br />
+<a href="http://tiger4th.com/">戻る</a><br />
+<br />
+
+<!-- AddThis Button BEGIN -->
+<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=300&amp;pubid=ra-4e3ab77310f2fc55"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e3ab77310f2fc55"></script>
+<!-- AddThis Button END -->
+<br /><br /><br />
+</div>
+</body>
+</html>
