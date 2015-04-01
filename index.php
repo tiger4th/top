96022fbc9e0ed0a2ab7223b17bba11d36cbbc6c2
tiger4th <tiger4th@gmail.com>
2015-04-02 01:20:56 +0900

    リニューアル

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="content-Style-type" content="text/css" />
-<meta http-equiv="content-Script-type" content="text/javascript" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>tiger4th.com</title>
-<link rel="stylesheet" type="text/css" href="./style.css" />
-<link rel="shortcut icon" href="./image/favicon.ico" type="image/vnd.microsoft.icon" />
-<link rel="icon" href="./image/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="shortcut icon" href="image/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="icon" href="image/favicon.ico" type="image/vnd.microsoft.icon" />
+<link href="./css/bootstrap.css" rel="stylesheet">
+<link href="./css/font-awesome.min.css" rel="stylesheet">
+<link href="./css/style.css" rel="stylesheet">
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
@@ -24,55 +25,172 @@
 </script>
 </head>
 <body>
+	<div class="top">
+	    <div class="container">
+	    	<div class="row">
+	    		<div class="col-lg-8 col-lg-offset-2 centered">
+	    			<h1>tiger4th.com</h1>
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
 
-<div class="centerMiddle"  align="center">
-<img src="./image/tiger4th.png" alt="tiger4th.com">
-<br /><br /><br />
-<a href="http://minrev.main.jp/"><img src="./image/minrev.png" alt="みんなの新着レビュー" height=50></a>
-<br /><br /><br />
-<a href="http://minrev.main.jp/c/"><img src="./image/minrevc.png" alt="みんなの新着レビューC" height=50></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/yamazon/"><img src="./image/yamazon.png" alt="Yamazon"></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/sc/"><img src="./image/sc.png" alt="social checker" height=50></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/anibuzz/"><img src="./image/anibuzz.png" alt="anibuzz" height=50></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/drabuzz/"><img src="./image/drabuzz.png" alt="drabuzz" height=50></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/hatebu/"><img src="./image/hatebu.png" alt="はてぶ新聞" height=60></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/hatebu_ex/"><img src="./image/hatebu_ex.png" alt="はてぶ新聞 速報版" height=60></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/sga/"><img src="./image/sga.png" alt="ソーシャルゲームオークション" height=30></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/auc/"><img src="./image/auc.png" alt="オークションシリーズ" height=30></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/snews/"><img src="./image/snews.png" alt="Social Newspaper" height=60></a>
-<br /><br /><br />
-<a href="http://tiger4th.com/sword/"><img src="./image/sword.png" alt="刀剣オークション" height=50></a>
-<br /><br /><br />
-<a href="others"><img src="./image/others.png" alt="others"></a>
-<br /><br /><br />
+	<div class="title">
+		<div class="container">
+			<div class="row centered">
+				<h2>SERVICES</h2>
+			</div>
+		</div>
+	</div>
 
-<br /><br /><br />
+	<div class="container mt">
+		<div class="row mt">
+			<h3 class="ml">SHOPPING</h3>
+			<div class="centered">
+				<div class="col-md-4">
+					<a href="http://minrev.main.jp/">
+						<div class="card">
+							<img src="image/minrev.png" alt="みんなの新着レビュー">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://minrev.main.jp/c/">
+						<div class="card">
+							<img src="image/minrevc.png" alt="みんなの新着レビューC">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/yamazon/">
+						<div class="card">
+							<img src="image/yamazon.png" alt="Yamazon">
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
 
-<img src="./image/gmail.png"alt="gmail">
-<br /><br /><br />
+		<div class="row">
+			<h3 class="ml">AUCTION</h3>
+			<div class="centered">
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/sga/">
+						<div class="card">
+							<img src="image/sga.png" alt="ソーシャルゲームオークション">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/auc/">
+						<div class="card">
+							<img src="image/auc.png" alt="オークションシリーズ">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/sword/">
+						<div class="card">
+							<img src="image/sword.png" alt="刀剣オークション">
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
 
-<div class="github">
-<iframe src="http://githubbadge.appspot.com/tiger4th?a=0" style="boAll your JSON-API belong to us!rder: 0;height: 142px;width: 200px;overflow: hidden;" frameBorder="0"></iframe>
-</div>
+		<div class="row">
+			<h3 class="ml">NEWS</h3>
+			<div class="centered">
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/hatebu/">
+						<div class="card">
+							<img src="image/hatebu.png" alt="はてぶ新聞">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/hatebu_ex/">
+						<div class="card">
+							<img src="image/hatebu_ex.png" alt="はてぶ新聞 速報版">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/snews/">
+						<div class="card">
+							<img src="image/snews.png" alt="Social Newspaper">
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
 
-<a href="https://twitter.com/tiger4th" class="twitter-follow-button" data-show-count="false">Follow @tiger4th</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-<br /><br /><br />
+		<div class="row">
+			<h3 class="ml">BUZZ</h3>
+			<div class="centered">
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/sc/">
+						<div class="card">
+							<img src="image/sc.png" alt="social checker">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/anibuzz/">
+						<div class="card">
+							<img src="image/anibuzz.png" alt="anibuzz">
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4">
+					<a href="http://tiger4th.com/drabuzz/">
+						<div class="card">
+							<img src="image/drabuzz.png" alt="drabuzz">
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
 
-<!-- AddThis Button BEGIN -->
-<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=300&amp;pubid=ra-4e3ab77310f2fc55"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e3ab77310f2fc55"></script>
-<!-- AddThis Button END -->
-<br /><br /><br />
-</div>
+		<div class="row mt centered">
+			<p class="mt"><a href="http://tiger4th.com/others/" class="btn btn-theme btn-lg">others</a></p>
+		</div>
+	</div>
+
+	<div class="title">
+		<div class="container">
+			<div class="row centered">
+				<h2>CONTACTS</h2>
+			</div>
+		</div>
+	</div>
+	
+	<div class="social">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="col-md-4 mt">
+						<i class="fa fa-twitter"></i><br><br>
+						<a href="https://twitter.com/tiger4th" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @tiger4th</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+					</div>
+					<div class="col-md-4 mt">
+						<i class="fa fa-github"></i><br><br>
+						<a aria-label="Follow @tiger4th on GitHub" data-style="mega" href="https://github.com/tiger4th" class="github-button">Follow @tiger4th</a>
+						<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
+					</div>
+					<div class="col-md-4 mt">
+						<i class="fa fa-envelope"></i><br><br>
+						<img src="image/gmail.png" alt="gmail" class="mail">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="share">
+		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e3ab77310f2fc55" async="async"></script>
+		<div class="addthis_sharing_toolbox"></div>
+	</div>
 </body>
 </html>
